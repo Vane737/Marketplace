fb6368ff375b9fd43e07b639d16e651ac837cba7
Vivian Vanessa Flores <vivian_vanessa_flores@hotmail.com>
2021-07-06 04:18:47 -0400

    version 1

---
+++ database/migrations/2021_06_21_200154_create_subcategories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubcategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->id();
+
+            $table->String('name');
+            $table->String('slug');
+            $table->String('image');
+            $table->boolean('color');
+            $table->boolean('size');
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategories');
+    }
+}
